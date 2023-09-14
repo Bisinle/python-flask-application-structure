bdac90c727ac4b1b808c1affce2a2102d5d69992
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-09-14 21:26:10 +0300

    build my first flask app

--- server/app.py
+++ server/app.py
@@ -3,3 +3,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>First Flask App</h1>'
+
+@app.route('/<string:myname>')
+def name(myname):
+    return f'<h4> App belongs to <h1>{myname}</h1>  </h4>'
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
